a5596a14e1e3b7500183b791353d647e1910aea5
John Park <176120986+jprk8@users.noreply.github.com>
2026-08-01 18:22:13 -0700

    Complete part 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -23,14 +23,18 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {total}</div>
-      <div>average {average}</div>
-      <div>positive {positive} %</div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={`${positive} %`} />
     </>
   )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return <div>{text} {value}</div>
 }
 
 const App = () => {
